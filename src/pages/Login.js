ef3ca195bd6c17e290e48eb7b66e510d9d4aefdc
pangidoannsh <pangidoanhrp13@gmail.com>
2022-11-18 12:03:11 +0800

    membuat halaman login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,69 +1,99 @@
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react';
+import Background from '../assets/images/login-bg.jpg'
+import { VehicleLogo, Shape, Logo, TopShape, BottomShape } from '../assets'
+import { Icon } from '@iconify/react';
 
-function Login() {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
-    const [isValid, setIsValid] = useState(false)
-    const [slide, setSlide] = useState({
-        onRight: false,
-        way: 'left-0'
-    });
 
-    const handleChange = e => {
-        switch (e.target.name) {
-            case 'email':
-                setEmail(e.target.value)
-                break
-            case 'password':
-                setPass(e.target.value)
-                break
-        }
+const Login = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    const [seePassword, setSeePassword] = useState(false);
+    const [passOnFocus, setPassOnFocus] = useState(false)
 
-    }
+    const screenHeight = window.innerHeight
+    const minMediumScreen = window.innerHeight < 680;
+
     const handleSubmit = e => {
         e.preventDefault()
+        console.log('test');
+    }
+    const handleContent = e => {
 
     }
-    const handleHover = () => {
-        let way = 'left-40'
-        if (slide.onRight) {
-            way = 'left-0'
-        }
-        setSlide({
-            onRight: !slide.onRight,
-            way
-        })
-    }
-
-    useEffect(() => {
-        if ((email !== '') && (pass !== '')) {
-            setIsValid(true)
-        } else {
-            setIsValid(false)
-        }
-    }, [email, pass]);
     return (
-        <div className='grid grid-cols-3 gap-20 justify-center  items-center h-96'>
-            <div className="col-span-1"></div>
-            <form onSubmit={handleSubmit} action="" className='col-span-1'>
-                <div className="flex flex-col gap-3">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" onChange={handleChange} name='email' id='email' value={email}
-                        placeholder='xxx@example.com' className='border border-black rounded-md py-2 px-1' />
+        <div className='h-screen bg-no-repeat bg-center bg-cover'
+            style={{ backgroundImage: `url(${Background})` }}>
+            <div className="flex">
+                <div className="w-[600px] h-screen">
+                    <div className={`fixed ${isOpen ? 'w-[600px]' : 'w-16'} duration-300
+                        relative h-screen bg-white rounded-r-2xl overflow-hidden`}>
+                        <div className="fixed z-10 top-[40px] left-16 duration-500">
+                            <img src={VehicleLogo} style={{
+                                width: minMediumScreen ? '120px' : '150px'
+                            }} />
+                        </div>
+                        <div className="absolute -top-[180px] -left-48" style={{
+                            width: minMediumScreen && '400px',
+                            top: minMediumScreen && '-150px',
+                            left: minMediumScreen && '-150px'
+                        }} >
+                            <img src={TopShape} />
+                        </div>
+                        <div className='absolute -bottom-60 -right-44'>
+                            <img src={BottomShape} />
+                        </div>
+                        <div className="px-16 h-full flex flex-col gap-y-6 justify-center">
+                            <div className="flex gap-x-6">
+                                <button className={`text-5xl text-dark-green font-medium`}>Sign In</button>
+                                <button className={`text-3xl text-slate-400 font-medium`} onClick={handleContent}>Sign up</button>
+                            </div>
+                            <form className='py-6' onSubmit={handleSubmit}>
+                                {/* Input Email */}
+                                <div className="relative mb-8 ">
+                                    <input id='email' type="text" className='focus:outline-none bg-transparent p-1 focus:rounded
+                                    border-b border-b-slate-400 w-full text-slate-400 focus:text-slate-600 peer ' placeholder=" " />
+                                    <div className='absolute bottom-0 duration-300 w-0 peer-focus:w-full h-[2px] bg-light-green'></div>
+                                    <label htmlFor="email" className="floating-label">Email</label>
+                                </div>
+                                {/* Input Password */}
+                                <div className="relative mb-3">
+                                    <input id='password' type={`${seePassword ? 'text' : 'password'}`} className='focus:outline-none
+                                    bg-transparent p-1 focus:rounded border-b border-b-slate-400 w-full text-slate-400 
+                                    focus:text-slate-600 peer' placeholder=" " onFocus={() => setPassOnFocus(true)} />
+                                    <div className='absolute bottom-0 duration-300 w-0 peer-focus:w-full h-[2px] bg-light-green '></div>
+                                    <label htmlFor="password" className="floating-label">Password</label>
+                                    <button id="seepassword" className={`absolute right-0 text-center p-2 pr-4 ${passOnFocus ? 'inline' : 'hidden'}`}
+                                        onClick={e => { e.preventDefault(); setSeePassword(!seePassword) }}>
+                                        <Icon icon={`${seePassword ? 'mdi:eye-off' : 'ic:round-remove-red-eye'}`}
+                                            className='text-lg text-slate-600' />
+                                    </button>
+                                </div>
+                                {/* Forgot Password */}
+                                <button className='text-sm font-medium text-slate-400 block mb-8 hover:text-light-green'>
+                                    forgot password?
+                                </button>
+                                {/* Login Button */}
+                                <button className='bg-light-green rounded py-2 px-4 text-white font-medium tracking-[.25em]'>
+                                    LOGIN
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col my-3">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" onChange={handleChange} name='password' id='password' value={pass}
-                        className='border border-black rounded-md py-2 px-1' />
+                <div className="flex-1 flex justify-center items-center h-screen">
+                    <img src={Logo} alt="" className='w-[582px]' />
                 </div>
-                <button onMouseEnter={handleHover}
-                    className={`relative duration-300 focus:bg-teal-700 bg-teal-500 col-span-1 rounded-md text-white py-2 px-4
-                     ${!isValid && slide.way}`}>
-                    Login
-                </button>
-            </form>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
+
+{/* <div className="relative z-0">
+                            <input type="text" id="email" className="block py-3 px-0 w-full text-sm bg-transparent border-0 border-b-2
+                                 border-gray-300 appearance-none dark:text-white focus:outline-none email" placeholder=" " />
+                            <label htmlFor="email" className="absolute text-sm text-white/60 duration-300 transform 
+                                -translate-y-6 scale-75 top-3 -z-10 origin-[0] email-focus:left-0 email-focus:text-blue-600 
+                                email-placeholder-shown:scale-100 email-placeholder-shown:translate-y-0 email-focus:scale-75 
+                                email-focus:-translate-y-6">Email</label>
+                        </div> */}
